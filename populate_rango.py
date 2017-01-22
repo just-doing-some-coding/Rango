7b6b841cb5c695bccd698bc5cb0ca4e3a7770275
Daniel Šmolík <2296043S@student.gla.ac.uk>
2017-01-22 18:52:12 +0000

    Change django /admin/ page

--- populate_rango.py
+++ populate_rango.py
@@ -35,12 +35,12 @@ def populate():
          "url": "http://flask.pocoo.org"}
     ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"views": 128, "likes": 64, "pages": python_pages},
+            "Django": {"views": 64, "likes": 32, "pages": django_pages},
+            "Other Frameworks": {"views": 32, "likes": 16, "pages": other_pages}}
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
             print("- {0} - {1}".format(str(c), str(p)))
@@ -54,8 +54,10 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
