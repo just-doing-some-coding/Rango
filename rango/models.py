27fae1427f3d6a4d329463a636c2a1a30580218b
Daniel Šmolík <2296043S@student.gla.ac.uk>
2017-01-24 22:12:53 +0000

    Added UserProfile model

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -36,3 +37,16 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
